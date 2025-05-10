236d46c172a49df046c461f1accdbb80874d176f
JEETVP <roberto.villegasrvo@gmail.com>
2025-05-09 23:01:23 -0600

    pantallas jeet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import QRScanner from './components/QRScanner';
-
+import Login from './components/login';
+import Register from './components/Register';
 function App() {
   return (
     <div>
 
-      <QRScanner />
+      <Register/>
     </div>
   );
 }
